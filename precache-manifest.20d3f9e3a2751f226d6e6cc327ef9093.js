1c61acf5dc5ea2f6cf1da1a7b4379a82cb1e6c63
Oussama Benchkroune <theblatanttech@gmail.com>
2020-08-30 18:01:37 +0100

    Updates

---
+++ precache-manifest.20d3f9e3a2751f226d6e6cc327ef9093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b582b3cf75ebc7fec08ae74679415b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf281e101c07df0dc6ce",
+    "url": "/static/css/main.879f5a45.chunk.css"
+  },
+  {
+    "revision": "214ac1b05fed99e4bf09",
+    "url": "/static/js/2.c0b586a5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c0b586a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf281e101c07df0dc6ce",
+    "url": "/static/js/main.983d1537.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
